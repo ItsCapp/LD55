f92366448bb002fef2a429c57108b171503ae86d
ItsCapp <93293747+ItsCapp@users.noreply.github.com>
2024-04-15 02:31:45 +0100

    day 2

--- assets/AntBehavior.gd
+++ assets/AntBehavior.gd
@@ -1,23 +1,39 @@
 extends Sprite2D
 
 var rng = RandomNumberGenerator.new()
+@onready var game_scene = get_node("/root/game_scene/TransparentWindow")
+@onready var foodicon = get_node("/root/game_scene/TransparentWindow")
+@onready var foodparticle = load("res://assets/game/foodeatparticle.tscn")
 
 var path = null
 var pathFollow = null
-var pathLength = 0
 var pathTime = 0
+@onready var speed = rng.randf_range(100, 150)
 
 var targetPos = Vector2(0, 0)
 var currentAction = "Idle"
+var currentlyCarrying = "Nothing"
+@onready var itemSprite = get_node("itemSprite")
 
-func newPath(startPos, targetPos):
+func newPath(startPos, endPos, curve):
 	path.curve.set_point_position(0, startPos)
-	path.curve.set_point_position(1, targetPos)
-	path.curve.set_point_out(0, Vector2(rng.randf_range(-10, 10), rng.randf_range(-10, 10)))
+	path.curve.set_point_position(1, endPos)
+	path.curve.set_point_out(0, Vector2(rng.randf_range(-curve, curve), rng.randf_range(-curve, curve)))
 	
-	pathLength = path.get_curve().get_baked_length()
 	pathTime = 0
 	pathFollow.progress = 0
+
+func changeCarrying(item):
+	if item == "random":
+		var possibleItems = ["food", "stone", "mushroom"]
+		item = possibleItems[rng.randi_range(0, 2)]
+	
+	if item == "food":
+		itemSprite.texture = load("res://assets/game/food.png")
+	elif item == "stone":
+		itemSprite.texture = load("res://assets/game/stone.png")
+	elif item == "mushroom":
+		itemSprite.texture = load("res://assets/game/mushroom.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,34 +43,104 @@ func _ready():
 	path.curve.add_point(Vector2(1, 1))
 	
 	pathFollow = PathFollow2D.new()
+	pathFollow.progress_ratio = 1
 	pathFollow.loop = false
+	
+	actioninterval = 5
 	
 	add_child(path)
 	path.add_child(pathFollow)
 
+func eat(amount):
+	if game_scene.food - amount < 0:
+		queue_free()
+	else:
+		game_scene.food -= amount
+	print("eat")
+	
+	var particle = foodparticle.instantiate()
+	particle.position = Vector2(0, 0)
+	particle.amount = amount
+	particle.emitting = true
+	add_child(particle)
+	await get_tree().create_timer(3).timeout
+	particle.queue_free()
+
 var actioninterval = 0
+var foodinterval = 0
 var actionwait = rng.randf_range(0.5, 5)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	foodinterval += delta
+	
+	if currentAction == "Idle":
+		if foodinterval > 60:
+			foodinterval = 0
+			eat(1)
+	elif currentAction == "Scavenging" or currentAction == "Collecting":
+		if foodinterval > 50:
+			foodinterval = 0
+			eat(1)
+	elif currentAction == "Fighting":
+		if foodinterval > 10:
+			foodinterval = 0
+			eat(1)
+	
 	if pathFollow.progress_ratio >= 0.99:
 		pathFollow.progress_ratio = 1
 		actioninterval += delta
+		
+		if currentlyCarrying == "Resources" and currentAction == "Scavenging":
+			currentlyCarrying = "Nothing"
+			itemSprite.texture = null
+			game_scene.food += rng.randi_range(0, 3)
+			game_scene.stone += rng.randi_range(0, 1)
 	else:
 		pathTime += delta
-		pathFollow.progress = pathTime * 100
+		pathFollow.progress = pathTime * speed
 	
 	if actioninterval > actionwait and currentAction == "Idle":
-		targetPos = Vector2(rng.randf_range(-1000, 1000), rng.randf_range(-1000, 1000))
-		actioninterval = 0
-		actionwait = rng.randf_range(0.5, 5)
+		targetPos = position + Vector2(rng.randf_range(-100, 100), rng.randf_range(-100, 100))
+		if targetPos.x > 400:
+			targetPos.x = 400
+		if targetPos.x < -400:
+			targetPos.x = -400
+		if targetPos.y > 400:
+			targetPos.y = 400
+		if targetPos.y < -400:
+			targetPos.y = -400
 		
-		newPath(position, targetPos)
+		actioninterval = 0
+		actionwait = rng.randf_range(0.5, 2.5)
+		
+		newPath(position, targetPos, 0)
 	elif actioninterval > actionwait and currentAction == "Scavenging":
-		targetPos = Vector2(1920, 1080)
+		var side = rng.randi_range(1, 4)
+		if side == 1:
+			targetPos = Vector2(rng.randf_range(-1060, 1060), -640)
+		elif side == 2:
+			targetPos = Vector2(rng.randf_range(-1060, 1060), 640)
+		elif side == 3:
+			targetPos = Vector2(-1060, rng.randf_range(-640, 640))
+		elif side == 4:
+			targetPos = Vector2(1060, rng.randf_range(-640, 640))
 		actioninterval = 0
-		actionwait = rng.randf_range(5, 10)
+		actionwait = rng.randf_range(15, 20)
 		
-		newPath(position, targetPos)
+		newPath(position, targetPos, 20)
+		currentAction = "Collecting"
+	elif actioninterval > actionwait and currentAction == "Collecting":
+		targetPos = Vector2(rng.randf_range(-50, 50), rng.randf_range(-50, 50))
+		actioninterval = 0
+		actionwait = rng.randf_range(1, 5)
+		
+		currentlyCarrying = "Resources"
+		changeCarrying("random")
+		
+		newPath(position, targetPos, 20)
+		currentAction = "Scavenging"
 	
 	position = lerp(position, pathFollow.position, 0.2)
 	rotation = lerp(rotation, pathFollow.rotation, 0.2)
+	
+	itemSprite.position = Vector2(position.x, position.y - 15)
--- assets/game_scene.gd
+++ assets/game_scene.gd
@@ -1,23 +1,47 @@
 extends Window
 
-var debugMode = true
+var debugMode = false
+var rng = RandomNumberGenerator.new()
 
 @onready var TransCam = get_node("/root/game_scene/TransparentWindow/TransCam") 
 @onready var AntNode = get_node("/root/game_scene/TransparentWindow/TransCam/Ants")
 @onready var antScene = load("res://ant.tscn")
+@onready var spawnParticle = load("res://assets/game/spawnParticle.tscn")
 
 var primaryScreen = {screenNum = DisplayServer.get_primary_screen(), size = DisplayServer.screen_get_size(DisplayServer.get_primary_screen()), position = DisplayServer.screen_get_position(DisplayServer.get_primary_screen())}
 @onready var game_scene = get_node("/root/game_scene")
 @onready var primaryWindow = game_scene.get_window()
+var centerWin = false
 
 var totalAnts = 0
 var totalIdle = 0
 var totalScavs = 0
 var totalFighters = 0
 
-func centerWindow(win:Window, tween:bool = false, duration:float = 0.5, easing:Tween.EaseType = Tween.EASE_IN_OUT, transtype:Tween.TransitionType = Tween.TRANS_LINEAR):
+var food = rng.randi_range(2, 4)
+var stone = 0
+
+func centerWindow(win:Window):
 	var targetpos = Vector2i(primaryScreen.position + ((primaryScreen.size / 2) - (win.size / 2)))
 	win.position = targetpos
+
+func resizeWindow(win:Window, xScale:int, yScale:int, tween:bool = false, duration:float = 0.5, easing:Tween.EaseType = Tween.EASE_IN_OUT, transtype:Tween.TransitionType = Tween.TRANS_LINEAR):
+	if xScale < 64:
+		xScale = 64
+	if yScale < 64:
+		yScale = 64
+	
+	var targetpos = Vector2i(((win.size.x - xScale) / 2), ((win.size.y - yScale) / 2))
+	var targetsize = Vector2i(xScale, yScale)
+	
+	if tween:
+		var tween_window = create_tween()
+		tween_window.set_parallel(true)
+		tween_window.tween_property(win, "position", win.position + targetpos, duration).set_ease(easing).set_trans(transtype)
+		tween_window.tween_property(win, "size", targetsize, duration).set_ease(easing).set_trans(transtype)
+	else:
+		win.position += targetpos
+		win.size = targetsize
 
 func countAnts():
 	totalAnts = 0
@@ -29,32 +53,66 @@ func countAnts():
 		totalAnts += 1
 		if ant.currentAction == "Idle":
 			totalIdle += 1
-		elif ant.currentAction == "Scavenging":
+		elif ant.currentAction == "Scavenging" or ant.currentAction == "Collecting":
 			totalScavs += 1
 		elif ant.currentAction == "Fighting":
 			totalFighters += 1
 
+func addAnts(default = false):
+	if food >= 5 or default:
+		var ant = antScene.instantiate()
+		AntNode.add_child(ant)
+		
+		if not default:
+			food -= 5
+		
+		var particle = spawnParticle.instantiate()
+		particle.emitting = true
+		if not default:
+			particle.position = Vector2(0, 0)
+		TransCam.add_child(particle)
+		
+		await get_tree().create_timer(3).timeout
+		particle.queue_free()
+
+func changeRole(from, to):
+	for ant in AntNode.get_children():
+		if ant.currentAction == from:
+			ant.currentAction = to
+			break
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	primaryWindow.get_viewport().size = Vector2i(primaryScreen.size.x, primaryScreen.size.y)
-	primaryWindow.size = Vector2i(320, 320)
+	primaryWindow.borderless = false
+	primaryWindow.size = Vector2i(64, 64)
+	primaryWindow.position = Vector2i(primaryScreen.position + ((primaryScreen.size / 2) - (primaryWindow.size / 2)))
+	resizeWindow(primaryWindow, 320, 320, true, 2, Tween.EASE_OUT, Tween.TRANS_BACK)
+	await get_tree().create_timer(2).timeout
+	centerWin = true
+	
+	for i in range(5):
+		await get_tree().create_timer(1).timeout
+		addAnts(true)
 
 func _input(event):
-	if debugMode == true:
-		if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel"):
 			get_tree().quit()
+	
+	if event.is_action_pressed("enableDebugMode"):
+		debugMode = true
+	
+	if debugMode == true:
 		if event.is_action_pressed("debugSummonAnt"):
 			var ant = antScene.instantiate()
-			
 			AntNode.add_child(ant)
+		if event.is_action_pressed("debugGiveFood"):
+			food += 1
+		if event.is_action_pressed("debugGiveStone"):
+			stone += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	centerWindow(primaryWindow)
+	if centerWin:
+		centerWindow(primaryWindow)
 	countAnts()
-
-func _onSummon():
-	print("pressed")
-	
-	var ant = antScene.instantiate()
-	AntNode.add_child(ant)
